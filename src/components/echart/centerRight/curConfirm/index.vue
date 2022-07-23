f55f023632b27feffa38802f9df24fdc9f87ea0e
fanglong4485 <fanglong4485@qq.com>
2022-07-23 22:20:51 +0800

    前端添加飞线数据。

--- src/components/echart/centerRight/curConfirm/index.vue
+++ src/components/echart/centerRight/curConfirm/index.vue
@@ -59,9 +59,70 @@ export default {
             value: 2,
           }
         ],
-        lineStyleArr: []
+        lineStyleArr: [],
+        lineDataArr: [
+          {
+            targetCity: "福州",
+            cityName: "莆田市",
+            value: 14.91000,
+          },
+          {
+            targetCity: "福州",
+            cityName: "厦门市",
+            value: 12.58000,
+          },
+          {
+            targetCity: "福州",
+            cityName: "南平市",
+            value: 11.55000,
+          },
+          {
+            targetCity: "福州",
+            cityName: "三明市",
+            value: 9.25000,
+          },
+          {
+            targetCity: "福州",
+            cityName: "泉州市",
+            value: 7.19000,
+          },
+          {
+            targetCity: "福州",
+            cityName: "漳州市",
+            value: 5.83000,
+          },
+          {
+            targetCity: "福州",
+            cityName: "宁德市",
+            value: 5.32000,
+          },
+          {
+            targetCity: "福州",
+            cityName: "龙岩市",
+            value: 3.22000,
+          },
+          {
+            targetCity: "莆田",
+            cityName: "福州市",
+            value: 39.79000,
+          },
+          {
+            targetCity: "莆田",
+            cityName: "泉州市",
+            value: 20.24000,
+          },
+          {
+            targetCity: "莆田",
+            cityName: "厦门市",
+            value: 10.78000,
+          },
+          {
+            targetCity: "莆田",
+            cityName: "漳州市",
+            value: 3.39000,
+          }
+        ]
       }
-
     }
   },
   //自定义isHistory属性，用于监听日期的变化
@@ -85,7 +146,40 @@ export default {
     Chart,
   },
   mounted() {
+    for(let item of this.cdata.lineDataArr){
+      this.cdata.lineStyleArr.push({
+        type: 'lines',
+        coordinateSystem: 'geo',
+        zlevel: 2,
+        //箭头图标
+        effect: {
+          show: true,
+          period: 10, //箭头指向速度，值越小速度越快
+          trailLength: 0.1, //特效尾迹长度[0,1]值越大，尾迹越长重
+          symbol: 'arrow', //箭头图标
+          symbolSize: item.value / 3 , //图标大小
+          color: 'rgba(245,225,17,0.5)', // 图标颜色
+        },
+        lineStyle: {
+          normal: {
+            show: true,
+            width: 2, //尾迹线条宽度
+            opacity: 0.2, //尾迹线条透明度
+            curveness: 0.3, //尾迹线条曲直度
+            color: 'rgba(238,214,15,0.5)', // 飞线颜色
+          },
+          color: '#ff0000',//这个属性好像没啥用
+        },
+        // 应该是传入全国数据集
+        //每个元素是一个数组，每个数组有出发点对象和目的点对象,第一个是出发点
+        data: [
+          [{coord: getCoord(item.cityName)},//泉州到福州
+            {coord: getCoord(item.targetCity)}
+          ]
+        ],
 
+      })
+    }
   },
   methods: {
     setCdata(){
